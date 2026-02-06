06345041af2fc14cdc4019c709a4bc71f5577513
Arunava-27 <drarunkundu22@gmail.com>
2026-02-06 16:27:48 +0530

    add comparison and symbol examples

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,26 @@
+console.log(2>1);
+console.log(2>=1);
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log("--------------------------------------");
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+
+console.log("2" === 2);
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id);
+console.log(anotherId);
+
+console.log();
+
+
