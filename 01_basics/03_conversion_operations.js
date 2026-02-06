4f796f1441afa1f97e9ade56bea86ed9eee3b5be
Arunava-27 <drarunkundu22@gmail.com>
2026-02-06 15:32:00 +0530

    add arithmetic and string operations examples

--- 01_basics/03_conversion_operations.js
+++ 01_basics/03_conversion_operations.js
@@ -26,3 +26,27 @@ let isLoggedIn = 1
 let getLoggedIn = Boolean(isLoggedIn)
 
 console.log(getLoggedIn);
+
+// *****************************Operations****************************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2**3);
+console.log(2/2);
+console.log(2%2);
+
+let str1 = "Hello";
+let str2 = " Arunava";
+
+console.log(str1+str2);
+
+let gameCounter = 100;
+++gameCounter;
+
+console.log(gameCounter);
