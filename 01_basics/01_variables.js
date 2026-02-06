e47288018e82c95605306db68ab1091f8421fabe
Arunava-27 <drarunkundu22@gmail.com>
2026-02-06 13:02:40 +0530

    add initial variable and datatype examples

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 1234567890;
+let accountName = "John Doe";
+var accountPassword = "123456"
+accountCity = "Kolkata"
+let accountState;
+
+// accountId = 2 // not allowed
+
+accountName = "Arunava"
+accountPassword = "654321"
+accountCity = "Mumbai"
+
+console.log(accountId);
+console.log(accountName);
+console.log(accountPassword);
+console.log(accountCity);
+
+// or
+
+console.table([accountId, accountName, accountPassword, accountCity, accountState])
+
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,8 @@
+"use strict"; // treat all JS code as newer version
+
+let num = 2222222222222222222222222222222222222
+ console.log(typeof num); // number
+ console.log(typeof null); // object
+ console.log(typeof undefined); // undefined
+ 
+ 
