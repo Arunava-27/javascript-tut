53a8fa3f65251b8b5abe2e3f54ae80583798d8dc
Arunava-27 <drarunkundu22@gmail.com>
2026-02-10 13:58:36 +0530

    add examples demonstrating stack and heap memory usage

--- 01_basics/memories.js
+++ 01_basics/memories.js
@@ -2,5 +2,32 @@
 // stack -> used by primitive datatype
 // heap -> used by Non-primitive datatype
 
+// in stack
+
+let str1 = "Arunava"
+let str2 = str1 // value copied
+str2 = "Rahul" // copied value changed
+
+console.log(str1);
+console.log(str2);
+
+console.log("----------------------------------");
+
+// in heap 
+
+let userOne = {
+    email: "user@email.com",
+    age: 22
+}
+
+let userTwo = userOne // reference passed
+
+userTwo.email = "arunava@email.com" // original value changed
+
+console.log(userOne);
+console.log(userTwo);
+
+
+
 
 
